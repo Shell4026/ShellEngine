a2e0a7c9219b66b8e429d1fbe7d34db1ac83fc10
Shell <104874910+Shell4026@users.noreply.github.com>
2024-03-25 15:33:28 +0900

    test: main.cpp - deltaTime

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,28 @@
 ﻿#include "Window/Window.h"
 
 #include <iostream>
+#include <chrono>
+#include <thread>
+
 int main(int arg, char** args[]) {
+	
 	sh::Window window;
 	window.Create(u8"테스트", 1024, 768);
 	
+	constexpr long long fps = static_cast<long long>(1000.0f / 144.0f);
+	
+	auto start = std::chrono::high_resolution_clock::now();
+	auto end = std::chrono::high_resolution_clock::now();
+	long long delta_time = 0;
 	while (window.IsOpen())
 	{
+		start = std::chrono::high_resolution_clock::now();
+		delta_time = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+		std::this_thread::sleep_for(std::chrono::milliseconds(fps - delta_time));
+		end = std::chrono::high_resolution_clock::now();
+		delta_time += std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+		//fmt::print("{}ms\n", delta_time);
+
 		sh::Event e;
 		while (window.PollEvent(e))
 		{
