b2adbdd4e8aa599db7f4c2b3c9fc19c98a44e48c
Shell4026 <rkddkwlenfdl@naver.com>
2024-04-03 03:54:15 +0900

    feat: Implement platform checking in the Util class

--- include/Core/Util.h
+++ include/Core/Util.h
@@ -5,6 +5,12 @@ namespace sh::core {
 	class Util
 	{
 	public:
+		enum class Platform {
+			Windows,
+			Linux,
+			Unknown
+		};
+	public:
 		static auto U8StringToWstring(const std::string& u8str)->std::wstring;
 		static constexpr bool IsDebug()
 		{
@@ -12,6 +18,15 @@ namespace sh::core {
 			return true;
 #else
 			return false;
+#endif
+		}
+		static constexpr auto GetPlatform()-> Platform {
+#if _WIN32
+			return Platform::Windows;
+#elif __linux__
+			return Platform::Linux;
+#else
+			return Platform::Unknown;
 #endif
 		}
 	};
