53512828183362911a347c2d2f3781123a49f676
Shell <104874910+Shell4026@users.noreply.github.com>
2024-04-11 15:19:01 +0900

    feat(Submit): Change function

--- include/Render/VulkanCommandBuffer.h
+++ include/Render/VulkanCommandBuffer.h
@@ -3,15 +3,22 @@
 #include "Export.h"
 
 #include <vulkan/vulkan.h>
+#include <functional>
 
 namespace sh::render {
 	class SH_RENDER_API CommandBuffer
 	{
 	private:
 		VkCommandBuffer buffer;
+	private:
+		auto Begin(VkCommandBufferBeginInfo* info = nullptr) -> VkResult;
+		auto End() -> VkResult;
 	public:
 		CommandBuffer(const VkDevice device, const VkCommandPool pool, const VkCommandBufferAllocateInfo* info = nullptr);
+		~CommandBuffer();
 
-		auto submit(VkQueue queue, const VkSubmitInfo* info = nullptr, const VkFence fence = VK_NULL_HANDLE) -> VkResult;
+		auto Submit(VkQueue queue, std::function<void()>& commands, const VkSubmitInfo* info = nullptr, const VkFence fence = VK_NULL_HANDLE) -> VkResult;
+
+		auto Reset() -> VkResult;
 	};
 }
--- src/Render/VulkanCommandBuffer.cpp
+++ src/Render/VulkanCommandBuffer.cpp
@@ -25,9 +25,63 @@ namespace sh::render {
 		assert(result == VkResult::VK_SUCCESS);
 	}
 
-	auto CommandBuffer::submit(VkQueue queue, const VkSubmitInfo* info, const VkFence fence) -> VkResult
+	CommandBuffer::~CommandBuffer()
+	{
+		Reset();
+	}
+
+	auto CommandBuffer::Begin(VkCommandBufferBeginInfo* info) -> VkResult
+	{
+		assert(buffer);
+
+		VkResult result;
+		if (!buffer)
+			return VkResult::VK_ERROR_UNKNOWN;
+
+		if (info)
+		{
+			result = vkBeginCommandBuffer(buffer, info);
+			assert(result == VkResult::VK_SUCCESS);
+		}
+
+		VkCommandBufferInheritanceInfo inheritInfo{};
+		inheritInfo.sType = VkStructureType::VK_STRUCTURE_TYPE_COMMAND_BUFFER_INHERITANCE_INFO;
+		inheritInfo.pNext = nullptr;
+		inheritInfo.renderPass = VK_NULL_HANDLE;
+		inheritInfo.subpass = 0;
+		inheritInfo.framebuffer = VK_NULL_HANDLE;
+		inheritInfo.occlusionQueryEnable = VK_FALSE;
+		inheritInfo.queryFlags = 0;
+		inheritInfo.pipelineStatistics = 0;
+
+		VkCommandBufferBeginInfo beginInfo{};
+		beginInfo.sType = VkStructureType::VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+		beginInfo.pNext = nullptr;
+		beginInfo.flags = 0;
+		beginInfo.pInheritanceInfo = &inheritInfo;
+
+		result = vkBeginCommandBuffer(buffer, info);
+		assert(result == VkResult::VK_SUCCESS);
+		return result;
+	}
+
+	auto CommandBuffer::End() -> VkResult
+	{
+		assert(buffer);
+
+		VkResult result = vkEndCommandBuffer(buffer);
+		assert(result == VkResult::VK_SUCCESS);
+
+		return result;
+	}
+
+	auto CommandBuffer::Submit(VkQueue queue, std::function<void()>& commands, const VkSubmitInfo* info, const VkFence fence) -> VkResult
 	{
 		VkResult result;
+		if (result = Begin(); result != VkResult::VK_SUCCESS) return result;
+		commands();
+		if (result = End(); result != VkResult::VK_SUCCESS) return result;
+
 		if (info)
 		{
 			vkQueueSubmit(queue, 1, info, fence);
@@ -50,5 +104,18 @@ namespace sh::render {
 		assert(result == VkResult::VK_SUCCESS);
 		result = vkQueueWaitIdle(queue);
 		return result;
+	}
+
+	auto CommandBuffer::Reset() -> VkResult
+	{
+		assert(buffer);
+		if (buffer)
+		{
+			VkResult result = vkResetCommandBuffer(buffer, VK_COMMAND_BUFFER_RESET_RELEASE_RESOURCES_BIT);
+			assert(result == VkResult::VK_SUCCESS);
+			return result;
+		}
+
+		return VkResult::VK_ERROR_UNKNOWN;
 	}
 }
