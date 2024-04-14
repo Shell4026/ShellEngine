5c569a8e1246e0611bededd86272f55524958584
Shell4026 <rkddkwlenfdl@naver.com>
2024-04-14 14:19:14 +0900

    feat(FileLoader): Impl FileLoader class

---
+++ include/Core/FileLoader.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include "Export.h"
+
+#include <vector>
+#include <string_view>
+#include <optional>
+
+namespace sh::core
+{
+	class FileLoader
+	{
+	protected:
+		std::vector<unsigned char> data;
+		std::string strData;
+	public:
+		SH_CORE_API virtual ~FileLoader();
+
+		SH_CORE_API auto LoadText(std::string_view dir) -> std::optional<const std::reference_wrapper<std::string>>;
+		SH_CORE_API auto LoadBinary(std::string_view dir) -> std::optional<const std::reference_wrapper<std::vector<unsigned char>>>;
+	};
+}
---
+++ src/Core/FileLoader.cpp
@@ -0,0 +1,48 @@
+﻿#include "FileLoader.h"
+
+#include <array>
+#include <algorithm>
+namespace sh::core
+{
+	FileLoader::~FileLoader()
+	{
+
+	}
+
+	auto FileLoader::LoadBinary(std::string_view dir) -> std::optional<const std::reference_wrapper<std::vector<unsigned char>>>
+	{
+		FILE* file;
+		fopen_s(&file, dir.data(), "rb");
+		if (!file)
+			return {};
+
+		std::array<unsigned char, 100> buffer{};
+		size_t size = 0;
+		while ((size = fread(buffer.data(), sizeof(unsigned char), 100, file)) > 0)
+		{
+			for (int i = 0; i < size; ++i)
+				data.push_back(buffer[i]);
+		}
+		fclose(file);
+
+		return std::ref(data);
+	}
+	auto FileLoader::LoadText(std::string_view dir) ->std::optional<const std::reference_wrapper<std::string>>
+	{
+		FILE* file;
+		fopen_s(&file, dir.data(), "r");
+		if (!file)
+			return {};
+
+		std::array<char, 100> buffer{};
+		size_t size = 0;
+		while ((size = fread(buffer.data(), sizeof(unsigned char), 100, file)) > 0)
+		{
+			for (int i = 0; i < size; ++i)
+				strData.push_back(buffer[i]);
+		}
+		fclose(file);
+
+		return std::ref(strData);
+	}
+}
