a3216060931664b411e289d83962cb33872b62c0
Shell4026 <rkddkwlenfdl@naver.com>
2024-04-28 08:58:29 +0900

    feat: Impl Mesh

---
+++ include/Render/Mesh.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+
+#include "Export.h"
+#include "IDrawable.h"
+
+#include <glm/glm.hpp>
+#include <vector>
+#include <initializer_list>
+
+namespace sh::render
+{
+	class Mesh : public IDrawable
+	{
+	private:
+		std::vector<glm::vec3> verts;
+	public:
+		SH_RENDER_API Mesh();
+		SH_RENDER_API Mesh(const Mesh& other);
+		SH_RENDER_API Mesh(Mesh&& other) noexcept;
+		SH_RENDER_API ~Mesh();
+
+		SH_RENDER_API auto operator=(const Mesh& other) -> Mesh&;
+		SH_RENDER_API auto operator=(Mesh&& other) noexcept -> Mesh&;
+
+		SH_RENDER_API void SetVertex(const std::vector<glm::vec3>& verts);
+		SH_RENDER_API void SetVertex(std::vector<glm::vec3>&& verts);
+		SH_RENDER_API void SetVertex(const std::initializer_list<glm::vec3>& verts);
+		SH_RENDER_API auto GetVertex() -> std::vector<glm::vec3>&;
+		SH_RENDER_API auto GetVertexConst() const -> const std::vector<glm::vec3>&;
+
+		SH_RENDER_API auto GetVertexCount() const -> int override;
+	};
+}
---
+++ src/Render/Mesh.cpp
@@ -0,0 +1,66 @@
+﻿#include "Mesh.h"
+
+namespace sh::render
+{
+	Mesh::Mesh()
+	{
+
+	}
+	Mesh::Mesh(const Mesh& other)
+	{
+		verts = other.verts;
+	}
+	Mesh::Mesh(Mesh&& other) noexcept
+	{
+		verts = std::move(other.verts);
+	}
+	Mesh::~Mesh()
+	{
+
+	}
+
+	auto Mesh::operator=(const Mesh& other)->Mesh&
+	{
+		verts = other.verts;
+
+		return *this;
+	}
+	auto Mesh::operator=(Mesh&& other) noexcept -> Mesh&
+	{
+		verts = std::move(other.verts);
+
+		return *this;
+	}
+
+	void Mesh::SetVertex(const std::vector<glm::vec3>& verts)
+	{
+		this->verts = verts;
+	}
+	void Mesh::SetVertex(std::vector<glm::vec3>&& verts)
+	{
+		this->verts = std::move(verts);
+	}
+	void Mesh::SetVertex(const std::initializer_list<glm::vec3>& verts)
+	{
+		this->verts.clear();
+		this->verts.resize(verts.size());
+		for (int i = 0; i < verts.size(); ++i)
+		{
+			this->verts[i] = *(verts.begin() + i);
+		}
+	}
+	auto Mesh::GetVertex() -> std::vector<glm::vec3>&
+	{
+		return verts;
+	}
+
+	auto Mesh::GetVertexConst() const -> const std::vector<glm::vec3>&
+	{
+		return verts;
+	}
+
+	auto Mesh::GetVertexCount() const -> int
+	{
+		return verts.size();
+	}
+}
