fd35ceb642df34ce27811f0f8ac75c9f24b4c168
Shell4026 <rkddkwlenfdl@naver.com>
2024-04-28 09:02:15 +0900

    feat: impl MeshRenderer component

---
+++ include/Game/Component/MeshRenderer.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "Component.h"
+
+#include "Game/Export.h"
+#include "Render/Mesh.h"
+
+namespace sh::game
+{
+	class MeshRenderer : public Component
+	{
+		SCLASS(MeshRenderer)
+	private:
+		const sh::render::Mesh* mesh;
+	public:
+		SH_GAME_API MeshRenderer(GameObject& owner);
+
+		SH_GAME_API void SetMesh(const sh::render::Mesh& mesh);
+		SH_GAME_API auto GetMesh() const -> const sh::render::Mesh&;
+
+		SH_GAME_API void Update() override;
+	};
+}
---
+++ src/Game/Component/MeshRenderer.cpp
@@ -0,0 +1,27 @@
+﻿#include "Component/MeshRenderer.h"
+#include "Render/Renderer.h"
+
+#include "GameObject.h"
+
+namespace sh::game
+{
+	MeshRenderer::MeshRenderer(GameObject& owner) :
+		Component(owner)
+	{
+	}
+
+	void MeshRenderer::SetMesh(const sh::render::Mesh& mesh)
+	{
+		this->mesh = &mesh;
+	}
+
+	auto MeshRenderer::GetMesh() const -> const sh::render::Mesh&
+	{
+		return *mesh;
+	}
+
+	void MeshRenderer::Update()
+	{
+		gameObject.world.renderer.PushDrawAble(mesh);
+	}
+}
