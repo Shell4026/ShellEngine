91040fdcf09dd92a86b0d6b60fc0c6d91bd2719a
Shell4026 <rkddkwlenfdl@naver.com>
2024-05-20 02:09:34 +0900

    feat(Transform): Impl transform

--- include/Game/Component/Transform.h
+++ include/Game/Component/Transform.h
@@ -4,6 +4,8 @@
 #include "Component.h"
 
 #include <glm/vec3.hpp>
+#include <glm/mat4x4.hpp>
+#include <glm/gtc/quaternion.hpp>
 
 namespace sh::game
 {
@@ -12,12 +14,35 @@ namespace sh::game
 		SCLASS(Transform)
 	private:
 		glm::vec3 localPosition;
-		glm::vec3 position;
+		glm::vec3 vPosition;
+		glm::vec3 vScale;
+		glm::vec3 vRot;
+
+		glm::mat4 matModel;
+
+		glm::quat quat;
+
+		bool updateMatrix;
+	private:
+		void UpdateMatrix();
+	public:
+		const glm::vec3& position;
+		const glm::vec3& scale;
+		const glm::vec3& rotation;
+		const glm::mat4& localToWorldMatrix;
 	public:
 		SH_GAME_API Transform(GameObject& owner);
 		SH_GAME_API ~Transform();
+		SH_GAME_API Transform(const Transform& other);
+		SH_GAME_API Transform(Transform&& other) noexcept;
 
 		SH_GAME_API void Awake() override;
 		SH_GAME_API void Start() override;
+		SH_GAME_API void Update() override;
+
+		SH_GAME_API void SetPosition(const glm::vec3& pos);
+		SH_GAME_API void SetScale(const glm::vec3& scale);
+		SH_GAME_API void SetRotation(const glm::vec3& rot);
+		SH_GAME_API void SetRotation(const glm::quat& rot);
 	};
 }
--- src/Game/Component/Transform.cpp
+++ src/Game/Component/Transform.cpp
@@ -1,27 +1,90 @@
 ﻿#include "Component/Transform.h"
 
-#include <iostream>
+#include "glm/mat4x4.hpp"
 
 namespace sh::game
 {
 	Transform::Transform(GameObject& owner) :
-		Component(owner)
-	{
+		Component(owner),
+		position(vPosition), scale(vScale), rotation(vRot), localToWorldMatrix(matModel),
 
+		localPosition(),
+		vPosition(glm::vec3(0.f, 0.f, 0.f)), vScale(glm::vec3(1.0f, 1.0f, 1.0f)), vRot(glm::vec3(0.f, 0.f, 0.f)),
+		matModel(), quat(vRot),
+		updateMatrix(false)
+	{
+		matModel = glm::translate(glm::mat4{1.0f}, vPosition) * glm::mat4_cast(quat) * glm::scale(glm::mat4{ 1.0f }, vScale);
 	}
 	Transform::~Transform()
 	{
 
 	}
 
+	Transform::Transform(const Transform& other) :
+		Component(other),
+		position(vPosition), scale(vScale), rotation(vRot), localToWorldMatrix(matModel),
+
+		localPosition(other.localPosition),
+		vPosition(other.vPosition), vScale(other.vScale), vRot(other.vRot),
+		matModel(other.matModel), quat(other.quat),
+		updateMatrix(other.updateMatrix)
+	{
+	}
+
+	Transform::Transform(Transform&& other) noexcept:
+		Component(std::move(other)),
+		position(vPosition), scale(vScale), rotation(vRot), localToWorldMatrix(matModel),
+
+		localPosition(std::move(other.localPosition)),
+		vPosition(std::move(other.vPosition)), vScale(std::move(other.vScale)), vRot(std::move(other.vRot)),
+		matModel(std::move(other.matModel)), quat(std::move(other.quat)),
+		updateMatrix(other.updateMatrix)
+	{
+	}
+
 	void Transform::Awake()
 	{
 		Super::Awake();
-		std::cout << "Transform Awake!\n";
 	}
 
 	void Transform::Start()
 	{
-		std::cout << "Transform Start!\n";
+	}
+
+	void Transform::Update()
+	{
+		if (updateMatrix)
+		{
+			UpdateMatrix();
+			updateMatrix = false;
+		}
+	}
+
+	void Transform::UpdateMatrix()
+	{
+		matModel = glm::translate(glm::mat4{ 1.0f }, vPosition) * glm::mat4_cast(quat) * glm::scale(glm::mat4{ 1.0f }, vScale);
+	}
+
+	void Transform::SetPosition(const glm::vec3& pos)
+	{
+		vPosition = pos;
+		updateMatrix = true;
+	}
+	void Transform::SetScale(const glm::vec3& scale)
+	{
+		vScale = scale;
+		updateMatrix = true;
+	}
+	void Transform::SetRotation(const glm::vec3& rot)
+	{
+		vRot = rot;
+		quat = glm::quat{ glm::radians(vRot) };
+		updateMatrix = true;
+	}
+	void Transform::SetRotation(const glm::quat& rot)
+	{
+		vRot = glm::degrees(glm::eulerAngles(quat));
+		quat = rot;
+		updateMatrix = true;
 	}
 }
