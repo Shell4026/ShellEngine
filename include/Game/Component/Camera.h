28f830a47692af3d2b92223cb77df3bca055cd14
Shell <104874910+Shell4026@users.noreply.github.com>
2024-05-20 15:11:46 +0900

    feat: Impl Camera component

---
+++ include/Game/Component/Camera.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#include "Component.h"
+#include "Game/Export.h"
+
+#include "glm/mat4x4.hpp"
+
+namespace sh::game
+{
+	class Camera : public Component
+	{
+		SCLASS(Camera)
+	private:
+		glm::mat4 matProj;
+		glm::mat4 matView;
+	public:
+		float fov;
+		float nearPlane;
+		float farPlane;
+
+		const glm::mat4& worldToCameraMatrix;
+	public:
+		SH_GAME_API Camera(GameObject& owner);
+		SH_GAME_API ~Camera() = default;
+		
+		SH_GAME_API void Start() override;
+		
+		SH_GAME_API auto GetProjMatrix() const -> const glm::mat4&;
+		SH_GAME_API auto GetViewMatrix() const -> const glm::mat4&;
+	};
+}
---
+++ src/Game/Component/Camera.cpp
@@ -0,0 +1,34 @@
+﻿#include "Component/Camera.h"
+#include "GameObject.h"
+
+#include "glm/gtc/matrix_transform.hpp"
+
+namespace sh::game
+{
+	Camera::Camera(GameObject& owner) :
+		Component(owner),
+		worldToCameraMatrix(matView),
+		matProj(), matView(),
+		fov(45.f), nearPlane(0.1f), farPlane(1000.f)
+	{
+	}
+
+	void Camera::Start()
+	{
+		matProj = glm::perspectiveFov(fov, 
+			static_cast<float>(gameObject.world.renderer.GetWidth()), 
+			static_cast<float>(gameObject.world.renderer.GetHeight()), 
+			nearPlane, farPlane);
+		matView = glm::lookAt(gameObject.transform->position, glm::vec3(0.f), glm::vec3(0.f, 1.0f, 0.f));
+	}
+
+	auto Camera::GetProjMatrix() const -> const glm::mat4&
+	{
+		return matProj;
+	}
+
+	auto Camera::GetViewMatrix() const -> const glm::mat4&
+	{
+		return matView;
+	}
+}
