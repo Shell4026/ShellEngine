3551f3599232a3bfcaed96e6bca620abb4592dd3
Shell <104874910+Shell4026@users.noreply.github.com>
2024-08-21 22:14:03 +0900

    Add: ContainerTest

---
+++ Test/ContainerTest.hpp
@@ -0,0 +1,44 @@
+﻿#pragma once
+
+#include "../include/Core/Container/SContainer.hpp"
+#include "../include/Core/SObject.h"
+
+#include <gtest/gtest.h>
+#include <chrono>
+#include <random>
+
+TEST(ContainerTest, VectorTest)
+{
+	class TestClass : public sh::core::SObject
+	{
+	public:
+		int num = 123;
+		TestClass() = default;
+	};
+
+	using namespace sh::core::container;
+
+	SSet<int, 128> container1;
+	std::set<int> container2;
+	auto start = std::chrono::high_resolution_clock::now();
+	for (int i = 0; i < 10000; ++i)
+	{
+		container1.insert(i);
+		if (i % 2 == 1)
+			container1.erase(i - 1);
+	}
+	auto end = std::chrono::high_resolution_clock::now();
+	auto time1 = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+
+	start = std::chrono::high_resolution_clock::now();
+	for (int i = 0; i < 10000; ++i)
+	{
+		container2.insert(i);
+		if (i % 2 == 1)
+			container2.erase(i - 1);
+	}
+	end = std::chrono::high_resolution_clock::now();
+	auto time2 = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+
+	EXPECT_LE(time1, time2);
+}
