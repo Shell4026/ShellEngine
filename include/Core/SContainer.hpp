ca0c86ffd8ae77487b6ad1e07a08a5956dfa4b2a
Shell <104874910+Shell4026@users.noreply.github.com>
2024-09-21 10:42:10 +0900

    chore: Change default container size

--- include/Core/SContainer.hpp
+++ include/Core/SContainer.hpp
@@ -13,16 +13,16 @@ namespace sh::core
 	template<typename T>
 	using SVector = std::vector<T>;
 
-	template<typename T, std::size_t defaultSize = 32, typename _Pr = std::less<T>>
+	template<typename T, std::size_t defaultSize = 8, typename _Pr = std::less<T>>
 	using SSet = std::set<T, _Pr, memory::SAllocator<T, defaultSize>>;
 
-	template<typename T, std::size_t defaultSize = 32, typename _hasher = std::hash<T>, typename _Keyeq = std::equal_to<T>>
+	template<typename T, std::size_t defaultSize = 8, typename _hasher = std::hash<T>, typename _Keyeq = std::equal_to<T>>
 	using SHashSet = std::unordered_set<T, _hasher, _Keyeq, memory::SAllocator<T, defaultSize>>;
 
-	template<typename KeyT, typename T, std::size_t defaultSize = 32, typename _Pr = std::less<KeyT>>
+	template<typename KeyT, typename T, std::size_t defaultSize = 8, typename _Pr = std::less<KeyT>>
 	using SMap = std::map<KeyT, T, _Pr, memory::SAllocator<std::pair<const KeyT, T>, defaultSize>>;
 
-	template<typename KeyT, typename T, std::size_t defaultSize = 32, typename _hasher = std::hash<KeyT>, typename _Keyeq = std::equal_to<KeyT>>
+	template<typename KeyT, typename T, std::size_t defaultSize = 8, typename _hasher = std::hash<KeyT>, typename _Keyeq = std::equal_to<KeyT>>
 	using SHashMap = std::unordered_map<KeyT, T, _hasher, _Keyeq, memory::SAllocator<std::pair<const KeyT, T>, defaultSize>>;
 
     /// @brief 잠금을 쓰지 않는 원자적 큐
