8278b07282796b81c7811ee65a750eb7be41b559
Shell <104874910+Shell4026@users.noreply.github.com>
2024-10-18 21:40:08 +0900

    refact(Vector): Using template to clean up code

--- UserCode/RotateObject.cpp
+++ UserCode/RotateObject.cpp
@@ -1,6 +1,7 @@
 ﻿#include "RotateObject.h"
 
 #include "Game/GameObject.h"
+#include "Game/Vector.h"
 
 using namespace sh::game;
 
@@ -20,6 +21,6 @@ SH_USER_API void RotateObject::OnEnable()
 
 SH_USER_API void RotateObject::Update()
 {
-	glm::vec3 rot = gameObject.transform->rotation;
-	gameObject.transform->SetRotation(rot + glm::vec3(0.0f, speed * world.deltaTime, 0.0f));
+	Vec3 rot = gameObject.transform->rotation;
+	gameObject.transform->SetRotation(rot + Vec3{ 0.0f, speed * world.deltaTime, 0.0f });
 }
--- include/Game/Vector.h
+++ include/Game/Vector.h
@@ -9,73 +9,187 @@
 #include "reactphysics3d/mathematics/Vector3.h"
 
 #include <cassert>
+#include <type_traits>
 
 namespace sh::game
 {
-	template<std::size_t N>
-	struct Vec
-	{
-		float data[N];
+    template <typename, typename = void>
+    struct HasX : std::false_type {};
+    template <typename T>
+    struct HasX<T, std::void_t<decltype(std::declval<T>().x)>> : std::true_type {};
 
-        auto operator=(const Vec<N>& other) -> Vec<N>&
+    template <typename, typename = void>
+    struct HasY : std::false_type {};
+    template <typename T>
+    struct HasY<T, std::void_t<decltype(std::declval<T>().y)>> : std::true_type {};
+
+    template <typename, typename = void>
+    struct HasZ : std::false_type {};
+    template <typename T>
+    struct HasZ<T, std::void_t<decltype(std::declval<T>().z)>> : std::true_type {};
+
+    template <typename, typename = void>
+    struct HasW : std::false_type {};
+    template <typename T>
+    struct HasW<T, std::void_t<decltype(std::declval<T>().w)>> : std::true_type {};
+    
+    struct Empty {};
+
+    template<std::size_t N>
+    struct Vec
+    {
+        union 
         {
-            for (std::size_t i = 0; i < N; ++i)
-                data[i] = other.data[i];
+            float data[N];
+            struct
+            {
+                std::conditional_t<N >= 1, float, Empty> x;
+                std::conditional_t<N >= 2, float, Empty> y;
+                std::conditional_t<N >= 3, float, Empty> z;
+                std::conditional_t<N >= 4, float, Empty> w;
+            };
+        };
+
+        operator glm::vec2() const
+        {
+            static_assert(N >= 1);
+
+            if constexpr (N >= 2)
+                return glm::vec2{ x, y };
+            else if constexpr (N >= 1)
+                return glm::vec2{ x, 0.f };
+        }
+        operator glm::vec3() const
+        {
+            static_assert(N >= 1);
+
+            if constexpr (N >= 3)
+                return glm::vec3{ x, y, z };
+            else if constexpr (N >= 2)
+                return glm::vec3{ x, y, 0.f };
+            else if constexpr (N >= 1)
+                return glm::vec3{ x, 0.f, 0.f };
+        }
+        operator glm::vec4() const
+        {
+            static_assert(N >= 1);
+
+            if constexpr (N >= 4)
+                return glm::vec4{ x, y, z, w };
+            else if constexpr (N >= 3)
+                return glm::vec4{ x, y, z, 0.f };
+            else if constexpr (N >= 2)
+                return glm::vec4{ x, y, 0.f, 0.f };
+            else if constexpr (N >= 1)
+                return glm::vec4{ x, 0.f, 0.f, 0.f };
+        }
+        operator reactphysics3d::Vector2() const
+        {
+            static_assert(N >= 1);
+
+            if constexpr (N >= 2)
+                return reactphysics3d::Vector2{ x, y };
+            else if constexpr (N >= 1)
+                return greactphysics3d::Vector2{ x, 0.f };
+        }
+        operator reactphysics3d::Vector3() const
+        {
+            static_assert(N >= 1);
+
+            if constexpr (N >= 3)
+                return reactphysics3d::Vector3{ x, y, z };
+            else if constexpr (N >= 2)
+                return reactphysics3d::Vector3{ x, y, 0.f };
+            else if constexpr (N >= 1)
+                return greactphysics3d::Vector3{ x, 0.f, 0.f };
+        }
+        auto operator=(const glm::vec2& other) -> Vec<N>
+        {
+            static_assert(N >= 1);
+
+            x = other.x;
+            if constexpr (N >= 2)
+                y = other.y;
+            return *this;
+        }
+        auto operator=(const glm::vec3& other) -> Vec<N>
+        {
+            static_assert(N >= 1);
+
+            x = other.x;
+            if constexpr (N >= 2)
+                y = other.y;
+            else if constexpr (N >= 3)
+                z = other.z;
+            return *this;
+        }
+        auto operator=(const glm::vec4& other) -> Vec<N>
+        {
+            static_assert(N >= 1);
+
+            x = other.x;
+            if constexpr (N >= 2)
+                y = other.y;
+            else if constexpr (N >= 3)
+                z = other.z;
+            else if constexpr (N >= 4)
+                w = other.w;
             return *this;
         }
 
         // 벡터 간 연산
-        auto operator+(const Vec<N>& other) const -> Vec<N>
+        auto operator+(const Vec<N> other) const-> Vec<N>
         {
             Vec<N> result{};
             for (std::size_t i = 0; i < N; ++i)
                 result.data[i] = this->data[i] + other.data[i];
             return result;
         }
-        auto operator-(const Vec<N>& other) const -> Vec<N>
+        auto operator-(const Vec<N> other) const-> Vec<N>
         {
             Vec<N> result{};
             for (std::size_t i = 0; i < N; ++i)
                 result.data[i] = this->data[i] - other.data[i];
             return result;
         }
-        auto operator*(const Vec<N>& other) const -> Vec<N>
+        auto operator*(const Vec<N> other) const-> Vec<N>
         {
             Vec<N> result{};
             for (std::size_t i = 0; i < N; ++i)
                 result.data[i] = this->data[i] * other.data[i];
             return result;
         }
-        auto operator/(const Vec<N>& other) const -> Vec<N>
+        auto operator/(const Vec<N> other) const-> Vec<N>
         {
             Vec<N> result{};
             for (std::size_t i = 0; i < N; ++i)
                 result.data[i] = this->data[i] / other.data[i];
             return result;
         }
+
         // 스칼라 연산
-        auto operator+(float scalar) const -> Vec<N>
+        auto operator+(float scalar) const-> Vec<N>
         {
             Vec<N> result{};
             for (std::size_t i = 0; i < N; ++i)
                 result.data[i] = this->data[i] + scalar;
             return result;
         }
-        auto operator-(float scalar) const -> Vec<N>
+        auto operator-(float scalar) const-> Vec<N>
         {
             Vec<N> result{};
             for (std::size_t i = 0; i < N; ++i)
                 result.data[i] = this->data[i] - scalar;
             return result;
         }
-        auto operator*(float scalar) const -> Vec<N>
+        auto operator*(float scalar) const-> Vec<N>
         {
             Vec<N> result{};
             for (std::size_t i = 0; i < N; ++i)
                 result.data[i] = this->data[i] * scalar;
             return result;
         }
-        auto operator/(float scalar) const -> Vec<N>
+        auto operator/(float scalar) const-> Vec<N>
         {
             assert(scalar != 0.f);
             Vec<N> result{};
@@ -83,34 +197,38 @@ namespace sh::game
                 result.data[i] = this->data[i] / scalar;
             return result;
         }
-	};
-    // 스칼라가 왼쪽
+    };
+
+    // 스칼라가 왼쪽인 연산
     template<std::size_t N>
-    static auto operator+(float scalar, const Vec<N>& vec) -> Vec<N>
+    auto operator+(float scalar, const Vec<N> vec)-> Vec<N>
     {
         Vec<N> result{};
         for (std::size_t i = 0; i < N; ++i)
             result.data[i] = scalar + vec.data[i];
         return result;
     }
+
     template<std::size_t N>
-    static auto operator-(float scalar, const Vec<N>& vec) -> Vec<N>
+    auto operator-(float scalar, const Vec<N> vec)-> Vec<N>
     {
         Vec<N> result{};
         for (std::size_t i = 0; i < N; ++i)
             result.data[i] = scalar - vec.data[i];
         return result;
     }
+
     template<std::size_t N>
-    static auto operator*(float scalar, const Vec<N>& vec) -> Vec<N>
+    auto operator*(float scalar, const Vec<N> vec)-> Vec<N>
     {
         Vec<N> result{};
         for (std::size_t i = 0; i < N; ++i)
             result.data[i] = scalar * vec.data[i];
         return result;
     }
+
     template<std::size_t N>
-    static auto operator/(float scalar, const Vec<N>& vec) -> Vec<N>
+    auto operator/(float scalar, const Vec<N> vec)-> Vec<N>
     {
         Vec<N> result{};
         for (std::size_t i = 0; i < N; ++i)
@@ -118,61 +236,7 @@ namespace sh::game
         return result;
     }
 
-    struct Vec2 : Vec<2>
-    {
-        float& x = data[0];
-        float& y = data[1];
-
-        SH_GAME_API Vec2();
-        SH_GAME_API Vec2(float x, float y);
-        SH_GAME_API Vec2(const Vec<2>& other);
-        SH_GAME_API Vec2(const glm::vec2& other);
-        SH_GAME_API Vec2(const reactphysics3d::Vector2& other);
-
-        SH_GAME_API operator glm::vec2() const;
-        SH_GAME_API operator reactphysics3d::Vector2() const;
-
-        SH_GAME_API auto operator=(const Vec<2>& other) -> Vec2&;
-        SH_GAME_API auto operator=(const Vec2& other) -> Vec2&;
-        SH_GAME_API auto operator=(const glm::vec2& other) -> Vec2&;
-        SH_GAME_API auto operator=(const reactphysics3d::Vector2& other) -> Vec2&;
-    };
-    struct Vec3 : Vec<3>
-    {
-        float& x = data[0];
-        float& y = data[1];
-        float& z = data[2];
-
-        SH_GAME_API Vec3();
-        SH_GAME_API Vec3(float x, float y, float z);
-        SH_GAME_API Vec3(const Vec<3>& other);
-        SH_GAME_API Vec3(const glm::vec3& other);
-        SH_GAME_API Vec3(const reactphysics3d::Vector3& other);
-
-        SH_GAME_API Vec3::operator glm::vec3() const;
-        SH_GAME_API Vec3::operator reactphysics3d::Vector3() const;
-
-        SH_GAME_API auto operator=(const Vec<3>& other) -> Vec3&;
-        SH_GAME_API auto operator=(const Vec3& other) -> Vec3&;
-        SH_GAME_API auto operator=(const glm::vec3& other) -> Vec3&;
-        SH_GAME_API auto operator=(const reactphysics3d::Vector3& other) -> Vec3&;
-    };
-    struct Vec4 : Vec<4>
-    {
-        float& x = data[0];
-        float& y = data[1];
-        float& z = data[2];
-        float& w = data[3];
-
-        SH_GAME_API Vec4();
-        SH_GAME_API Vec4(float x, float y, float z, float w);
-        SH_GAME_API Vec4(const Vec<4>& other);
-        SH_GAME_API Vec4(const glm::vec4& other);
-
-        SH_GAME_API operator glm::vec4() const;
-
-        SH_GAME_API auto operator=(const Vec<4>& other) -> Vec4&;
-        SH_GAME_API auto operator=(const Vec4& other) -> Vec4&;
-        SH_GAME_API auto operator=(const glm::vec4& other) -> Vec4&;
-    };
+    using Vec2 = Vec<2>;
+    using Vec3 = Vec<3>;
+    using Vec4 = Vec<4>;
 }//namespace
