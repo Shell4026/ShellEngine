afd673e7643079809da841539c4403af913aa446
Shell <104874910+Shell4026@users.noreply.github.com>
2024-12-26 05:28:11 +0900

    feat: Implement SpinLock

---
+++ include/Core/SpinLock.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include "Export.h"
+
+#include <atomic>
+namespace sh::core
+{
+	class SpinLock
+	{
+	private:
+		std::atomic_flag flag;
+	public:
+		SH_CORE_API SpinLock();
+
+		SH_CORE_API void Lock();
+		SH_CORE_API void UnLock();
+	};
+}//namespace
---
+++ src/Core/SpinLock.cpp
@@ -0,0 +1,22 @@
+﻿#include "PCH.h"
+#include "SpinLock.h"
+
+#include <thread>
+
+namespace sh::core
+{
+	SpinLock::SpinLock()
+	{
+	}
+	SH_CORE_API void SpinLock::Lock()
+	{
+		while (flag.test_and_set(std::memory_order::memory_order_acquire))
+		{
+			std::this_thread::yield();
+		}
+	}
+	SH_CORE_API void SpinLock::UnLock()
+	{
+		flag.clear(std::memory_order::memory_order_release);
+	}
+}//namespace
