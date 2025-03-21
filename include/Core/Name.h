568041aaa9a80bb7112e9d3344ad40ca192ba8e6
Shell <104874910+Shell4026@users.noreply.github.com>
2025-03-21 16:23:47 +0900

    feat: Implement Name class

---
+++ include/Core/Name.h
@@ -0,0 +1,44 @@
+﻿#pragma once
+#include "Export.h"
+
+#include <string>
+#include <mutex>
+#include <unordered_map>
+namespace sh::core
+{
+	/// @brief 비교가 빠른 문자열 클래스. 스레드 안전하다.
+	class Name
+	{
+	private:
+		SH_CORE_API static std::unordered_map<std::string, int> dic;
+		SH_CORE_API static std::mutex mu;
+		friend class std::hash<sh::core::Name>;
+
+		const std::string* ptr;
+	public:
+		SH_CORE_API Name(const std::string& str);
+		SH_CORE_API Name(const char* str);
+		SH_CORE_API ~Name();
+
+		SH_CORE_API auto operator==(const Name& other) const -> bool;
+		SH_CORE_API auto operator==(const std::string& str) const -> bool;
+		SH_CORE_API auto operator!=(const Name& other) const -> bool;
+		SH_CORE_API auto operator!=(const std::string& str) const -> bool;
+		SH_CORE_API      operator const std::string& () const;
+
+		SH_CORE_API auto GetString() const -> const std::string&;
+	};
+}//namespace
+
+namespace std
+{
+	template<>
+	struct std::hash<sh::core::Name>
+	{
+		auto operator()(const sh::core::Name& name) const -> std::size_t
+		{
+			std::hash<const void*> hasher{};
+			return hasher(reinterpret_cast<const void*>(name.ptr));
+		}
+	};
+}
---
+++ src/Core/Name.cpp
@@ -0,0 +1,70 @@
+﻿#include "Name.h"
+
+namespace sh::core
+{
+	std::unordered_map<std::string, int> Name::dic{};
+	std::mutex Name::mu{};
+
+	Name::Name(const std::string& str)
+	{
+		mu.lock();
+		auto it = dic.find(str);
+		if (it == dic.end())
+			ptr = &dic.insert_or_assign(str, 1).first->first;
+		else
+		{
+			ptr = &it->first;
+			++it->second;
+		}
+		mu.unlock();
+;	}
+	Name::Name(const char* _str)
+	{
+		mu.lock();
+		std::string str(_str);
+		auto it = dic.find(str);
+		if (it == dic.end())
+			ptr = &dic.insert_or_assign(std::move(str), 1).first->first;
+		else
+		{
+			ptr = &it->first;
+			++it->second;
+		}
+		mu.unlock();
+	}
+	Name::~Name()
+	{
+		mu.lock();
+		auto it = dic.find(*ptr);
+		if (--it->second == 0)
+			dic.erase(it);
+		mu.unlock();
+	}
+	SH_CORE_API auto Name::operator==(const Name& other) const -> bool
+	{
+		return ptr == other.ptr;
+	}
+	SH_CORE_API auto Name::operator==(const std::string& str) const -> bool
+	{
+		auto it = dic.find(str);
+		if (it == dic.end())
+			return false;
+		return ptr == &it->first;
+	}
+	SH_CORE_API auto Name::operator!=(const Name& other) const -> bool
+	{
+		return  ptr != other.ptr;
+	}
+	SH_CORE_API auto Name::operator!=(const std::string& str) const -> bool
+	{
+		return !operator==(str);
+	}
+	SH_CORE_API Name::operator const std::string& () const
+	{
+		return *ptr;
+	}
+	SH_CORE_API auto Name::GetString() const -> const std::string&
+	{
+		return *ptr;
+	}
+}//namespace
