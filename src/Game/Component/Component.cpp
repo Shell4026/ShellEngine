50f46306575f09e43ca10da9b2f81d3f3d80c775
Shell <104874910+Shell4026@users.noreply.github.com>
2025-09-05 07:24:24 +0900

    refact(Component): Remove unnecessary code

--- src/Game/Component/Component.cpp
+++ src/Game/Component/Component.cpp
@@ -108,34 +108,29 @@ namespace sh::game
 	{
 		core::Json mainJson{ Super::Serialize() };
 		const core::reflection::STypeInfo* type = &GetType();
-		while (type)
+
+		while (type != nullptr)
 		{
-			core::Json json{};
-			for (auto& prop :type->GetProperties())
+			core::Json* json = nullptr;
+			if (!mainJson.contains(type->name))
+				mainJson[type->name] = core::Json::object();
+			json = &mainJson[type->name];
+
+			for (auto& prop : type->GetProperties())
 			{
 				if (prop->bNoSaveProperty)
 					continue;
+
 				const core::reflection::TypeInfo& propType = prop->type;
 				const core::Name& name = prop->GetName();
-				if (propType == core::reflection::GetType<int>() || prop->isEnum)
-					core::SerializeProperty(json, name, *prop->Get<int>(*this));
-				else if (propType == core::reflection::GetType<float>())
-					core::SerializeProperty(json, name, *prop->Get<float>(*this));
-				else if (propType == core::reflection::GetType<std::string>())
-					core::SerializeProperty(json, name, *prop->Get<std::string>(*this));
-				else if (propType == core::reflection::GetType<bool>())
-					core::SerializeProperty(json, name, *prop->Get<bool>(*this));
-				else if (propType == core::reflection::GetType<Vec4>())
-					core::SerializeProperty(json, name, *prop->Get<Vec4>(*this));
+
+				if (propType == core::reflection::GetType<Vec4>())
+					core::SerializeProperty(*json, name, *prop->Get<Vec4>(*this));
 				else if (propType == core::reflection::GetType<Vec3>())
-					core::SerializeProperty(json, name, *prop->Get<Vec3>(*this));
+					core::SerializeProperty(*json, name, *prop->Get<Vec3>(*this));
 				else if (propType == core::reflection::GetType<Vec2>())
-					core::SerializeProperty(json, name, *prop->Get<Vec2>(*this));
-				else if (prop->isSObjectPointer)
-					core::SerializeProperty(json, name, *prop->Get<SObject*>(*this));
+					core::SerializeProperty(*json, name, *prop->Get<Vec2>(*this));
 			}
-			if (!json.empty())
-				mainJson[type->name] = json;
 			type = type->GetSuper();
 		}
 		return mainJson;
@@ -159,27 +154,7 @@ namespace sh::game
 				const core::reflection::TypeInfo& propType = prop->type;
 				const core::Name& name = prop->GetName();
 
-				if (propType == core::reflection::GetType<int>() || prop->isEnum)
-				{
-					if (core::DeserializeProperty(compJson, name, *prop->Get<int>(*this)))
-						OnPropertyChanged(*prop.get());
-				}
-				else if (propType == core::reflection::GetType<float>())
-				{
-					if (core::DeserializeProperty(compJson, name, *prop->Get<float>(*this)))
-						OnPropertyChanged(*prop.get());
-				}
-				else if (propType == core::reflection::GetType<std::string>())
-				{
-					if (core::DeserializeProperty(compJson, name, *prop->Get<std::string>(*this)))
-						OnPropertyChanged(*prop.get());
-				}
-				else if (propType == core::reflection::GetType<bool>())
-				{
-					if (core::DeserializeProperty(compJson, name, *prop->Get<bool>(*this)))
-						OnPropertyChanged(*prop.get());
-				}
-				else if (propType == core::reflection::GetType<Vec4>())
+				if (propType == core::reflection::GetType<Vec4>())
 				{
 					if (core::DeserializeProperty(compJson, name, *prop->Get<Vec4>(*this)))
 						OnPropertyChanged(*prop.get());
@@ -192,11 +167,6 @@ namespace sh::game
 				else if (propType == core::reflection::GetType<Vec2>())
 				{
 					if (core::DeserializeProperty(compJson, name, *prop->Get<Vec2>(*this)))
-						OnPropertyChanged(*prop.get());
-				}
-				else if (prop->isSObjectPointer)
-				{
-					if (core::DeserializeProperty(compJson, name, *prop->Get<SObject*>(*this)))
 						OnPropertyChanged(*prop.get());
 				}
 			}
