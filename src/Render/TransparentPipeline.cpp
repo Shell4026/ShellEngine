835b6b90819ef4a911e5bfeee5d95fe83e918ed6
Shell <104874910+Shell4026@users.noreply.github.com>
2025-09-13 05:06:26 +0900

    feat(TransparentPipeline): Sorting by Camera Distance

--- src/Render/TransparentPipeline.cpp
+++ src/Render/TransparentPipeline.cpp
@@ -1,4 +1,8 @@
 ﻿#include "TransparentPipeline.h"
+#include "Camera.h"
+
+#define GLM_ENABLE_EXPERIMENTAL
+#include "glm/gtx/norm.hpp"
 
 #include <algorithm>
 namespace sh::render
@@ -16,19 +20,36 @@ namespace sh::render
 	}
 	SH_RENDER_API void TransparentPipeline::RecordCommand(const std::vector<const Camera*>& cameras, uint32_t imgIdx)
 	{
-		if (bAddedDrawable)
+		std::vector<const Camera*> camVector;
+		camVector.reserve(cameras.size());
+		for (auto cam : cameras)
 		{
+			if (cam == nullptr)
+				continue;
+			if (auto it = std::find(ignoreCameras.begin(), ignoreCameras.end(), cam); it == ignoreCameras.end())
+				camVector.push_back(cam);
+		}
+		if (camVector.size() == 0)
+			return;
+
+		for (auto cam : camVector)
+		{
+			const auto& camPos = cam->GetPos(core::ThreadType::Render);
 			for (auto& renderGroup : renderGroups)
 			{
 				std::sort(renderGroup.drawables.begin(), renderGroup.drawables.end(),
-					[](const Drawable* left, const Drawable* right) -> bool
+					[camPos](const Drawable* left, const Drawable* right) -> bool
 					{
-						return left->GetPriority() > right->GetPriority();
+						const glm::vec3 posLeft = left->GetModelMatrix(core::ThreadType::Render)[3];
+						const glm::vec3 posRight = right->GetModelMatrix(core::ThreadType::Render)[3];
+						float leftLenSqr = glm::distance2(posLeft, camPos);
+						float rightLenSqr = glm::distance2(posRight, camPos);
+						return leftLenSqr > rightLenSqr;
 					}
 				);
 			}
-			bAddedDrawable = false;
+
+			impl->RecordCommand(passName, *cam, renderGroups, imgIdx);
 		}
-		RenderPipeline::RecordCommand(cameras, imgIdx);
 	}
 }//namespace
