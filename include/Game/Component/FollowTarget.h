bfc5ca02fae1f1bff95222505ff7e1a6e3129230
Shell <104874910+Shell4026@users.noreply.github.com>
2026-03-04 15:54:23 +0900

    feat: Implement FollowTarget component

---
+++ include/Game/Component/FollowTarget.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "Game/Export.h"
+#include "Game/Component/Component.h"
+
+namespace sh::game
+{
+	class Transform;
+	class FollowTarget : public Component
+	{
+		COMPONENT(FollowTarget)
+	public:
+		SH_GAME_API FollowTarget(GameObject& owner);
+
+		SH_GAME_API void Update() override;
+	private:
+		PROPERTY(target, core::PropertyOption::sobjPtr)
+		Transform* target = nullptr;
+		PROPERTY(speed)
+		float speed = 1.f;
+	};
+}//namespace
---
+++ src/Game/Component/FollowTarget.cpp
@@ -0,0 +1,29 @@
+﻿#include "Game/Component/FollowTarget.h"
+#include "Game/Component/Transform.h"
+#include "Game/World.h"
+
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/norm.hpp>
+namespace sh::game
+{
+	FollowTarget::FollowTarget(GameObject& owner) :
+		Component(owner)
+	{
+	}
+	SH_GAME_API void FollowTarget::Update()
+	{
+		if (!core::IsValid(target))
+			return;
+
+		glm::vec3 pos = gameObject.transform->GetWorldPosition();
+		glm::vec3 targetPos = target->GetWorldPosition();
+
+		const float disSqr = glm::distance2(pos, targetPos);
+		if (disSqr < 1e-6)
+			return;
+
+		pos = glm::mix(pos, targetPos, speed * world.deltaTime);
+		gameObject.transform->SetWorldPosition(pos);
+		gameObject.transform->UpdateMatrix();
+	}
+}//namespace
