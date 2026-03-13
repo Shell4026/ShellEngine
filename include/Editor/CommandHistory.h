e8b1e09e6cbc81d76b664e9a23a6e574a5fafda8
Shell <104874910+Shell4026@users.noreply.github.com>
2026-03-13 23:35:34 +0900

    feat(CommandHistory): Add undo limit

--- include/Editor/CommandHistory.h
+++ include/Editor/CommandHistory.h
@@ -5,7 +5,7 @@
 #include <memory>
 #include <string>
 #include <string_view>
-#include <vector>
+#include <deque>
 
 namespace sh::editor
 {
@@ -24,20 +24,25 @@ namespace sh::editor
 		SH_EDITOR_API void EndTransaction();
 		SH_EDITOR_API void CancelTransaction();
 
+		SH_EDITOR_API void SetUndoLimit(uint32_t limit) { undoLimit = limit; }
+
 		SH_EDITOR_API auto CanUndo() const -> bool;
 		SH_EDITOR_API auto CanRedo() const -> bool;
 		SH_EDITOR_API auto IsTransactionOpen() const -> bool;
 
 		SH_EDITOR_API auto GetUndoName() const -> std::string_view;
 		SH_EDITOR_API auto GetRedoName() const -> std::string_view;
+		SH_EDITOR_API auto GetUndoLimit() const -> uint32_t { return undoLimit; }
 	private:
 		class CompositeCommand;
 
 		void PushExecutedCommand(std::unique_ptr<EditorCommand>&& command);
 		void ClearRedoStack();
 	private:
-		std::vector<std::unique_ptr<EditorCommand>> undoStack;
+		std::deque<std::unique_ptr<EditorCommand>> undoStack;
 		std::vector<std::unique_ptr<EditorCommand>> redoStack;
 		std::unique_ptr<CompositeCommand> currentTransaction;
+
+		uint32_t undoLimit = 50;
 	};
 }//namespace
